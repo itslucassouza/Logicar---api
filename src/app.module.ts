4dbefd1ced6701e8f77f44c2b353c1cd3a38cd7e
itslucassouza <lucasvini193@hotmail.com>
2023-10-20 01:21:41 -0300

    adjust conflicts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,9 @@
 import { Module } from "@nestjs/common";
 import { UsersModule } from "./modules/users/users.module";
 import { DatabaseModule } from "./shared/database/database.module";
-import { ParkingModule } from './modules/parking/parking.module';
 import { CarModule } from './modules/Car/car.module';
 import { TransactionModule } from './modules/transaction/transaction.module';
+import { ParkingModule } from "./modules/Parking/parking.module";
 
 
 @Module({
